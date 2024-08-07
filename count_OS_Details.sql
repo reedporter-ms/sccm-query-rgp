1350833f15fda18a9558f9fe169d0b0ecea27f3e
Reed Porter <v-reedporter@microsoft.com>
2024-08-07 15:16:22 -0500

    Update count_OS_Details.sql

--- count_OS_Details.sql
+++ count_OS_Details.sql
@@ -9,9 +9,9 @@ business interruption, loss of business information, or other pecuniary loss) ar
 sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages. 
 */
 
-Select Count (*) as 'Count'
- 
-      ,BuildExt as 'Version'
+Select Count (*) as 'Count' 
+      ,BuildExt as 'Build'
+      ,[Operating_System_Name_and0] as 'OS'
 
 FROM [CM_PS1].[dbo].[v_R_System]
 Where  Active0 = 1
